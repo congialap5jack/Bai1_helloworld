0e0daff2eedd28bac5c20de44cd4966b2239a7f9
congialap <MAY 06@LAB2-09>
2024-11-04 11:27:29 +0700

    Add project files.

---
+++ Bai1_helloworld.cpp
@@ -0,0 +1,19 @@
+// Bai1_helloworld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Hello World!\n";
+	cout << "Toi ten la " << "\tNguyen Tuan Kiet" << endl;
+	cout << "Toi Dang Hoc \tc++ " << endl;
+	return 0;
+}
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
